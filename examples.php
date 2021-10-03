e42788e514d85b4e296df5e89bbfafa4854122c1
Nathan Green <91598981+AIRADLABS@users.noreply.github.com>
2021-10-03 01:06:54 -0400

    Add files via upload

---
+++ examples.php
@@ -0,0 +1,158 @@
+<?
+// *********************************************************************
+// SOLBOT-PHP EXAMPLES (c) 2021 AIRAD LABS INC.
+// VERSION: 0.3 (pre-release)
+// This code is licensed under MIT license (see LICENSE.txt for details)
+// *********************************************************************
+
+// *********************************************************************
+// For development we are allowing the creation of new pods on our dev network. 
+// This effectively bypasses the ownership verification of an associated NFT.
+// 
+// Click the link below to generate a new pod.
+// http://dev.solbots.io/bypass/bypass.php
+// 
+// The output will contain your new pod address and pod key.
+// Copy the output to a notepad!
+// Example Output:
+// 
+// {
+//     "status": "success",
+//     "message": "Pod Online!",
+//     "data": {
+//         "token": "b0316c27ef755c1434db35ea9d6da2ed92a50c6f20f61ab5be21a40332ff856e",
+//         "mint": "6cce36d9f8a9e151b100234af75cca89d55bcb94c153f51847debdf1f39cae45",
+//         "signature": "7a220893a5674a153f1fad576112ad5871a3b1dcb5a22988e6719a74a68fbd8c",
+//         "properties": false,
+//         "owner": "0bb400349c8a42345b5573c6666bee3efe768712bed2268f5bcd03eaf7a89636",
+//         "expires": 1664756091.720955,
+//         "pod": "solbot.2UjZXTXVUT2ZMSlFaST06OnAE8yRzVBb3R2UjZXTXVUT2ZMSlFaST06OnAGNShbOz1RvXgegZtwWCE=", // This is the pod address!
+//         "user": false,
+//         "claimed": 1633220091.720955,
+//         "status": "online",
+//         "updated": 1633220091.720955,
+//         "created": 1633220091.702492,
+//         "key": "solbot.1633220091.7210.3c4afb2638dfb2sfb2638d33aab42638dda9dda64be5afb2638dfb2sfb2638d33aab4297" // This is the pod key!
+//     }
+// }
+//
+// *********************************************************************
+
+// *********************************************************************
+header('Content-Type: application/json');
+// *********************************************************************
+
+// *********************************************************************
+// import and start solbot (on the dev network)
+require("solbot.0.3.php");
+// $solbot = new solbot( "dev.solbots.io" , "YOUR_ADDRESS_HERE", "YOUR_KEY_HERE" );
+// *********************************************************************
+
+// *********************************************************************
+// output the default construct (only use for development)
+// $result = $solbot->object();
+// $array = json_decode( $result );
+// $result = null;
+// print_r($array);
+// *********************************************************************
+
+// *********************************************************************
+// get network details // verifys you have access to the network
+// $result = $solbot->info();
+// *********************************************************************
+
+// *********************************************************************
+// create folders in your pod // a practice folder tree
+// $result = $solbot->newFolder("folder_1");
+// $result = $solbot->newFolder("folder_1/folder_2");
+// $result = $solbot->newFolder("folder_1/folder_2/folder_3");
+// *********************************************************************
+
+// *********************************************************************
+// create json files for each folder (for structured data)
+// $result = $solbot->newFile("folder_1/json_1");
+// $result = $solbot->newFile("folder_1/folder_2/json_2");
+// $result = $solbot->newFile("folder_1/folder_2/folder_3/json_3");
+// *********************************************************************
+// create a txt files for each folder (for non-structured data)
+// $result = $solbot->newFile("folder_1/txt_1","txt");
+// $result = $solbot->newFile("folder_1/folder_2/txt_2","txt");
+// $result = $solbot->newFile("folder_1/folder_2/folder_3/txt_3","txt");
+// *********************************************************************
+
+// *********************************************************************
+// overwrite json file 1
+// $data = new stdClass();
+// $data->test = "Hi JSON File 1";
+// $result = $solbot->write("folder_1/json_1","json",$data);
+// *********************************************************************
+// overwrite text file 1
+// $data = "Hi TXT File 1";
+// $result = $solbot->write("folder_1/txt_1","txt",$data);
+// *********************************************************************
+
+// *********************************************************************
+// get new deadbolt
+// $result = $solbot->deadboltGet();
+// *********************************************************************
+// deadbolt json file 1
+// $result = $solbot->deadbolt("folder_1/json_1","f235b567d010eef2e0075de5be0800a5a5eec63f11936d206c9b35080dc64b1");
+// *********************************************************************
+// read deadbolted json file 1
+// $result = $solbot->read("folder_1/json_1","f235b567d010eef2e0075de5be0800a5a5eec63f11936d206c9b35080dc64b1");
+// *********************************************************************
+// *********************************************************************
+// remove deadbolt
+// $result = $solbot->deadboltRemove("folder_1/json_1","f235b567d010eef2e0075de5be0800a5a5eec63f11936d206c9b35080dc64b1");
+// *********************************************************************
+
+// *********************************************************************
+// read json file 1
+// $result = $solbot->read("folder_1/json_1",);
+// *********************************************************************
+// read txt file 1
+// $result = $solbot->read("folder_1/txt_1","txt");
+// *********************************************************************
+// read json file 1 file and return with the data still encrypted
+// $result = $solbot->read("folder_1/json_1",false,true);
+// *********************************************************************
+
+// *********************************************************************
+// rename json file 3 to "funny_json_3"
+// $result = $solbot->rename("folder_1/folder_2/folder_3/json_3","funny_json_3","json");
+// *********************************************************************
+// rename txt file 3 to "funny_txt_3"
+// $result = $solbot->rename("folder_1/folder_2/folder_3/txt_3","funny_txt_3","txt");
+// *********************************************************************
+// rename folder_3 to "funny_folder"
+// $result = $solbot->rename("folder_1/folder_2/folder_3","funny_folder");
+// *********************************************************************
+
+// *********************************************************************
+// move json file 2 to root
+// $result = $solbot->move("folder_1/folder_2/json_2",false,"json");
+// *********************************************************************
+// move text file 2 to folder 1
+// $result = $solbot->move("folder_1/folder_2/txt_2","folder_1","txt");
+// *********************************************************************
+// move folder_3 to root
+// $result = $solbot->move("folder_1/folder_2/funny_folder");
+// *********************************************************************
+
+// *********************************************************************
+// copy json file 2
+// $result = $solbot->copy("json_2","json");
+// *********************************************************************
+// copy text file 2
+// $result = $solbot->copy("folder_1/txt_2","txt");
+// *********************************************************************
+// copy folder 2
+// $result = $solbot->copy("folder_1/folder_2");
+// *********************************************************************
+
+// *********************************************************************
+// list folder with options
+// $result = $solbot->list("folder_1",array("filter"=>"folders","sort"=>"name","order"=>"asc"));
+// *********************************************************************
+
+if(isset($result)){echo $result;}
---
+++ solbot.0.3.php
@@ -0,0 +1,283 @@
+<?
+////////////////////////////////////////////////////////////////////////
+// SOLBOT-PHP: (c) 2021 AIRAD LABS INC.
+// VERSION: 0.3 (pre-release)
+// This code is licensed under MIT license (see LICENSE.txt for details)
+////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////
+// solbot class
+class solbot {
+////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////
+// initial object construct after instantiation
+public function __construct( $pod_network , $pod_address , $pod_key ) {
+$this->protocol = (!empty($_SERVER['HTTPS'])&&$_SERVER['HTTPS']!=='off'||$_SERVER['SERVER_PORT']==443)?"https://":"http://";
+$this->network = $pod_network;
+$this->version = "0.3";
+if(!isset($this->payload)){
+$this->payload = new stdClass;
+}
+$this->payload->pod_address = $pod_address;
+$this->payload->pod_key = $pod_key;
+}
+////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////
+// dev only // remove before live deployment
+public function object() {
+return json_encode( $this );  
+}
+////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////
+// curl post // used by each method to send the payload to the network
+public function send() {
+$path = $this->protocol.$this->network;
+$ch = curl_init( $path );
+if( isset( $this->payload->command ) ) {
+$path = $path."/".$this->payload->command.str_replace(".","-",$this->version)."/";
+$data = json_encode( $this->payload );
+$data = array( "payload" => $data );
+$data = http_build_query( $data );
+$options = array( 
+  CURLOPT_URL => $path , 
+  CURLOPT_RETURNTRANSFER => true ,
+  CURLOPT_HTTPHEADER => array('Content-Type:application/x-www-form-urlencoded','Content-Length:'.strlen( $data ) ), 
+  CURLOPT_POST => 1 , 
+  CURLOPT_POSTFIELDS => $data 
+);
+}
+curl_setopt_array( $ch , $options );
+$result = curl_exec( $ch );
+curl_close( $ch );
+//return stripslashes($result);
+return $result;
+}
+////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////
+// returns details about the network you're currently connected to
+public function info() {
+$this->payload->command = "info/";
+return $this->send();
+}
+////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////
+// creates a new folder
+public function newFolder( $pod_folder="" ) {
+$this->payload->command = "newFolder/";
+$this->payload->pod_folder = $pod_folder;
+return $this->send();
+}
+////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////
+// creates a new json or txt file // json by default
+public function newFile( $pod_file="" , $pod_format="json" ) {
+$this->payload->command = "newFile/";
+$this->payload->pod_file = $pod_file;
+$this->payload->pod_format = $pod_format;
+return $this->send();
+}
+////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////
+// reads a file // pass third param false to return non-encrypted data
+public function read( $pod_file="" , $format=false , $decrypt=true ) {
+$this->payload->command = "read/";
+$this->payload->pod_file = $pod_file;
+if( $format == "json" or $format == "txt" ){
+$this->payload->pod_format = $format;
+}
+elseif( $format !== false ){
+$this->payload->pod_deadbolt = $format;
+}
+if( $decrypt == false ){
+$this->payload->pod_decrypt = false;
+}
+return $this->send();
+}
+////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////
+// overwrites the contents of a file // false = json or specify txt
+public function write( $pod_file="" , $pod_format="json" , $pod_data ) {
+$this->payload->command = "write/";
+$this->payload->pod_file = $pod_file;
+$this->payload->pod_format = $pod_format;
+$this->payload->pod_data = $pod_data;
+return $this->send();
+}
+////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////
+// returns a randomaly generated deadbolt for deadbolting json files
+public function deadboltGet() {
+$this->payload->command = "deadboltGet/";
+return $this->send();
+}
+////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////
+// deadbolts a json file // file will be read only
+public function deadbolt( $pod_file=false , $pod_deadbolt=false ) {
+if( $pod_deadbolt == false ){
+return array("status"=>"error","message"=>"No Deadbolt Supplied!");
+}
+elseif( $pod_file == false ){
+return array("status"=>"error","message"=>"No File Supplied!");
+}
+else{
+$this->payload->command = "deadbolt/";
+$this->payload->pod_deadbolt = $pod_deadbolt;
+$this->payload->pod_file = $pod_file;
+return $this->send();
+}
+}
+////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////
+// removes a deadbolt from a json file
+// warning! // providing the wrong deadbolt key will corrupt the file 
+public function deadboltRemove( $pod_file="" , $deadbolt=false ) {
+if( $deadbolt == false ){
+return array("status"=>"error","message"=>"No Deadbolt Supplied!");
+}
+else{
+$this->payload->command = "deadboltRemove/";
+$this->payload->pod_deadbolt = $deadbolt;
+$this->payload->pod_file = $pod_file;
+return $this->send();
+}
+}
+////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////
+// lists the contents of a folder // defaults to root
+public function list( $pod_folder=false , $options=false ) {
+$this->payload->command = "list/";
+if( $pod_folder !== "root" and $pod_folder !== false ) {
+$this->payload->pod_folder = $pod_folder;
+}
+if( $options !== false and is_array( $options ) ) {
+  foreach( $options as $key => $val ){
+    if( $key == "filter" ){
+      $this->payload->pod_filter = $val;
+    }
+    elseif( $key == "format" ){
+      $this->payload->pod_format = $val;
+    }
+    elseif( $key == "sort" ){
+      $this->payload->pod_sort = $val;
+    }
+    elseif( $key == "order" ){
+      $this->payload->pod_order = $val;
+    }
+  }
+}
+return $this->send();
+}
+////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////
+// renames a folder // or any txt or json file if a format is specified
+public function rename( $renamefrom=false , $renameto=false , $format=false ) {
+$this->payload->command = "rename/";
+
+if( $renamefrom == false or $renameto == false ){
+$result = array( "status" => "error" , "message" => "No filename provided!" );
+}
+else{
+$this->payload->pod_renamefrom = $renamefrom;
+$this->payload->pod_renameto = $renameto;
+}
+
+if( $format == "json" ){
+$this->payload->pod_format = $format;
+}
+elseif( $format == "txt" ){
+$this->payload->pod_format = $format;
+}
+
+return $this->send();
+
+if( isset( $result["status"] ) ){
+//return $result;
+}
+else{
+//return $this->send();
+}
+
+
+
+}
+////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////
+// copies a folder // or any txt or json file if a format is specified
+public function copy( $pod_path=false , $pod_format=false ) {
+
+$this->payload->command = "copy/";
+
+if( $pod_path == false ){
+$result = array( "status" => "error" , "message" => "No file or folder specified!" );
+}
+else{
+$this->payload->pod_path = $pod_path;
+}
+
+if( $pod_format == "json" ){
+$this->payload->pod_format = $pod_format;
+}
+elseif( $pod_format == "txt" ){
+$this->payload->pod_format = $pod_format;
+}
+
+if( isset( $result["status"] ) ){
+return $result;
+}else{
+return $this->send();
+}
+
+}
+////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////
+// moves a folder // or any txt or json file if a format is specified
+public function move( $movefrom=false , $moveto=false , $format=false ) {
+$this->payload->command = "move/";
+
+if( $movefrom == false ){
+$result = array( "status" => "error" , "message" => "No filename provided!" );
+}
+else{
+$this->payload->pod_movefrom = $movefrom;
+}
+
+if( $movefrom !== false ){
+$this->payload->pod_moveto = $moveto;
+}
+
+if( $format == "json" ){
+$this->payload->pod_format = $format;
+}
+elseif( $format == "txt" ){
+$this->payload->pod_format = $format;
+}
+
+if( isset( $result["status"] ) ){
+return $result;
+}
+else{
+return $this->send();
+}
+
+}
+////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////
+}
+////////////////////////////////////////////////////////////////////////
